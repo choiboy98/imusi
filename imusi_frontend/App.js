df914dbac293210cfc32b78a8297aac1ec53ea5e
Daniel Choi <daniel.won.choi@gmail.com>
2018-11-03 04:29:57 -0500

    camera done

--- imusi_frontend/App.js
+++ imusi_frontend/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Login from './Scenes/Login';
+import Camera from './Scenes/CameraScene'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Login />
+      //<Login />
+      <Camera />
     );
   }
 }
---
+++ imusi_frontend/Scenes/CameraScene.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+
+import { Camera, Permissions } from 'expo';
+import { MaterialIcons, Ionicons } from '@expo/vector-icons';
+
+export default class CameraScene extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+    flash: false,
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ 
+      hasCameraPermission: status === 'granted' 
+    });
+  }
+
+  flipCamera = () => {
+    this.setState({
+      type: this.state.type === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back,
+    });
+  }
+
+  snap = async() => {
+    if (this.camera) {
+      let photo = await this.camera.takePictureAsync();
+      console.log(photo);
+    }
+  }
+
+  setCameraRef = (ref) => {
+    this.camera = ref;
+  } 
+
+  render() {
+    if (this.state.hasCameraPermission === null) {
+      return <View />;
+    } else if (this.state.hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+
+      let logo_size = 50;
+
+      return (
+        <View style={styles.container}>
+          <Camera style={styles.container} type={this.state.type} ref = {this.setCameraRef}>
+            <View style={styles.buttonContainer}>
+
+              <MaterialIcons style = {styles.buttonDummy} name = "camera" size = {logo_size} color = "white" />
+              
+              <TouchableOpacity
+                style={styles.button}
+                onPress={this.snap}>
+                <MaterialIcons name = "camera" size = {logo_size} color = "white" />
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={this.flipCamera}>
+                <Ionicons name = "ios-reverse-camera" size = {logo_size} color = "white" />
+              </TouchableOpacity>
+
+
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttonDummy: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 10,
+    color: 'transparent'
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 18, 
+    marginBottom: 10, 
+    color: 'white'
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+  }
+});
