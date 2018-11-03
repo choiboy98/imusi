571dda5308da9ff6d4f6fa608802535a0a3ccd80
Brandon Wang <sourbrandon@gmail.com>
2018-11-03 13:44:50 -0500

    added byte to arr converter

--- model/model.py
+++ model/model.py
@@ -3,10 +3,10 @@ from vibrance import calc_vibrance
 from chaoticness import calc_chaos
 from popularity import calc_popularity
 from valence import calc_valence
+from image_utils import bytes_to_rgb
 
-
-def calculate_image_vector(image, concepts):
-    image_arr = None
+def calculate_image_vector(image_bytes, concepts):
+    image_arr = bytes_to_rgb(image_bytes)
     t = calc_image_temperature(image_arr)
     v = calc_vibrance(image_arr)
     c = calc_chaos(concepts)
