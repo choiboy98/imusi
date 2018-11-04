27158529a5f2bc6a7847018d8e954ba7ac11eebb
Thomas Yang <tyang1780@gmail.com>
2018-11-04 01:41:53 -0500

    spotify react

--- imusi_frontend/App.js
+++ imusi_frontend/App.js
@@ -1,15 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, WebView, Linking } from 'react-native';
 
 import Login from './Scenes/Login';
 import Camera from './Scenes/CameraScene'
+//import Spotify from './spotify_api'
+
+
+var SpotifyWebApi = require('spotify-web-api-node');
+var scopes = ['user-read-private', 'user-read-email'],
+    redirectUri = 'http://localhost:8888/callback/',
+    clientId = 'c38558d936cf40cfbf11a34fcecaa36a';
+
+// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+var spotifyApi = new SpotifyWebApi({
+  redirectUri: redirectUri,
+  clientId: clientId
+});
+
+// Create the authorization URL
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes);
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      uri: undefined
+    }
+  }
+
   render() {
-    return (
-      //<Login />
-      <Camera />
-    );
+    if (authorizeURL === undefined) {
+      return (<View/>);
+    } else {
+      return (
+        <WebView
+          ref={(ref) => { this.webview = ref; }}
+          source={{ uri: authorizeURL }}
+          onNavigationStateChange={(event) => {
+            if (event.url !== authorizeURL) {
+              this.webview.stopLoading();
+              Linking.openURL(event.url);
+            }
+          }}
+        />
+      );
+    }
   }
 }
 
