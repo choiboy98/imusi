9c366f5c6f12120ec2bb0bf8e5fae9be71003c75
Brandon Wang <sourbrandon@gmail.com>
2018-11-04 00:51:13 -0500

    scaling image down

--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+import cv2
+
 from model.temperature import calc_image_temperature #acoustiness
 from model.vibrance import calc_vibrance # danceability
 from model.chaoticness import calc_chaos # energy
@@ -5,11 +7,18 @@ from model.popularity import calc_popularity # speechiness
 from model.valence import calc_valence # valence
 from model.image_utils import bytes_to_rgb
 
+MAX_WIDTH = 250
+
 def calculate_image_vector(image_bytes, concepts):
     import sys
     print('here1')
     sys.stdout.flush()
     image_arr = bytes_to_rgb(image_bytes)
+    scale = 1
+    if image_arr.shape[0] > MAX_WIDTH:
+        scale = MAX_WIDTH / image_arr.shape[0]
+    image_arr = cv2.resize(image_array, (0, 0), fx=scale, fy=scale)
+
     print('here2')
     sys.stdout.flush()
     temperature = calc_image_temperature(image_arr)
